4be94e4ea250013ae3bf6300cd7135753265a23c
sleeplesseditor <josh@bryson.me.uk>
2018-03-13 18:32:36 +0000

    Database query added

--- server/main.js
+++ server/main.js
@@ -1,5 +1,19 @@
+import _ from 'lodash';
 import { Meteor } from 'meteor/meteor';
+import { Employees } from '../imports/collections/employees';
+import { image, helpers } from 'faker';
 
 Meteor.startup(() => {
-    
+    const numberRecords = Employees.find({}).count();
+    //Check for pre-existing data in collections
+    if (!numberRecords) {
+        _.times(5000, () => {
+            const { name, email, phone } = helpers.createCard();
+
+            Employees.insert({
+                name, email, phone, 
+                avatar: image.avatar()
+            });
+        });
+    }
 });
