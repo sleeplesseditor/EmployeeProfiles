e1ff45c3918133b5b01e9af4d746b7e74d358c8a
sleeplesseditor <josh@bryson.me.uk>
2018-03-14 00:03:49 +0000

    Load more... function refined

--- client/components/EmployeeList.js
+++ client/components/EmployeeList.js
@@ -6,6 +6,15 @@ import EmployeeDetail from './EmployeeDetail';
 const PER_PAGE = 20;
 
 class EmployeeList extends Component {
+    componentWillMount() {
+        this.page = 1;
+    }
+
+    handleButtonClick() {
+        Meteor.subscribe('employees', PER_PAGE * (this.page + 1));
+        this.page += 1;
+    }
+
     render() {
         return (
             <div>
@@ -14,7 +23,7 @@ class EmployeeList extends Component {
                         <EmployeeDetail key={employee._id} employee={employee}/>
                     )}
                 </div>
-                <button onClick={() => Meteor.subscribe('employees', 40)} 
+                <button onClick={this.handleButtonClick.bind(this)} 
                     className="btn btn-primary">
                     Load More...
                 </button>
